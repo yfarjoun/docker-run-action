19a68104dfbb24c1fef1d44b7f356b25a88f3672
Yossi Farjoun <farjoun@broadinstitute.org>
2021-03-30 12:51:53 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS $INPUT_IMAGE "`cat semicolon_delimited_script`"
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS $INPUT_SHELL $INPUT_IMAGE "`cat semicolon_delimited_script`"
