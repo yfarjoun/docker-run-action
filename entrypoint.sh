d81d15ad68ab9415cf8832f4cfc258b798607bc4
Yossi Farjoun <farjoun@broadinstitute.org>
2021-04-03 15:54:46 -0400

    only replace entrypoint if shell argument is non-empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,8 @@ if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+if [ ! -z $INPUT_SHELL ];
+then INPUT_OPTIONS="$INPUT_OPTIONS --entrypoint=$INPUT_SHELL"
+fi
+
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS $INPUT_IMAGE -c "`cat semicolon_delimited_script`"
