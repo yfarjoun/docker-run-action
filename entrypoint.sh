b7642bd7d5c58be25037d356435f45f60840f535
Yossi Farjoun <farjoun@broadinstitute.org>
2021-04-13 13:15:03 -0400

    new design

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,4 @@ if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-COMMAND_PREFIX="-c"
-
-if [ "$INPUT_USE_SHELL_ENTRYPOINT" = "false" ]; then 
-	COMMAND_PREFIX=""
-	INPUT_SHELL=""	
-fi
-
-if [ ! -z $INPUT_SHELL ];
-then INPUT_OPTIONS="$INPUT_OPTIONS --entrypoint=$INPUT_SHELL"
-fi
-
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS $INPUT_IMAGE $COMMAND_PREFIX "`cat semicolon_delimited_script`"
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS $INPUT_IMAGE $INPUT_SHELL $INPUT_SCRIPT_PREFIX "`cat semicolon_delimited_script`"
